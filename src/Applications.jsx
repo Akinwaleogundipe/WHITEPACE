0aba5400f56665437c912152df236eb8c9ddefa0
Akinwaleogundipe <akinwaleogundipe245@gmail.com>
2025-06-27 19:37:51 +0100

    Responsive HeroPage

--- src/Applications.jsx
+++ src/Applications.jsx
@@ -6,38 +6,34 @@ import bgelem from "./assets/desktop/bgelem.png";
 import Apps from "./assets/desktop/Apps.png";
 
 
-function Applications() {
+export default function Applications() {
 	return (
-		<>
-			<main className=" flex flex-col  mx-auto  relative bg-[#043873] text-white">
-				<img
-					src={bgelem}
-					alt=""
-					className="absolute object-cover inset-0 w-full h-full "
-				/>
+    <>
+      <main className=" flex flex-col  mx-auto  relative bg-[#043873] text-white">
+        <img
+          src={bgelem}
+          alt=""
+          className="absolute object-cover inset-0 w-full h-full "
+        />
 
-				<section className="flex flex-col md:flex-row justify-center items-center z-10 gap-18 my-20 md:my-35">
-					<div className=" ">
-						<img src={Apps} alt="" className=" w-full h-full " />
-        </div>
-					<div className="w-full lg:w-2/5 text-center md:text-start">
-						<p className=" font-bold text-4xl md:text-6xl lg:text-7xl leading-[100%]">
-            Work with Your Favorite Apps Using whitepace
-          </p>
-						<p className=" mt-6 text-lg">
-							Whitepace teams up with your favorite software. Integrate with
-							over 1000+ apps with Zapier to have all the tools you need for
-							your project success
-          </p>
-          <button className="bg-[#4F9CF9] self-center max-w-[194px] h-[63px] rounded-xl flex place-content-center items-center text-xl gap-2 ">
-            Read More <GoArrowRight />
-          </button>
-        </div>
-      </section>
-    </main>
-  </>
-);
-
-}
-
-export default Applications;
+        <section className="flex flex-col md:flex-row justify-center items-center z-10 gap-18 my-20 mx-4 md:mx-8 md:my-35">
+          <div className=" ">
+            <img src={Apps} alt="" className="  " />
+          </div>
+          <div className=" flex flex-col w-full lg:w-2/5 text-center md:text-start gap-8 ">
+            <p className=" font-bold text-4xl md:text-6xl lg:text-7xl lg:leading-20">
+              Work with Your Favorite Apps Using whitepace
+            </p>
+            <p className=" mt-6 text-lg">
+              Whitepace teams up with your favorite software. Integrate with
+              over 1000+ apps with Zapier to have all the tools you need for
+              your project success
+            </p>
+            <button className="bg-[#4F9CF9] self-center md:self-start  w-40  h-16 rounded-xl flex place-content-center items-center text-xl gap-2 ">
+              Read More <GoArrowRight />
+            </button>
+          </div>
+        </section>
+      </main>
+    </>
+  );}
--- src/HeroPage.jsx
+++ src/HeroPage.jsx
@@ -1,12 +1,10 @@
 import logo from "../src/assets/Logo.png";
-import "./index.css";
 import { RiArrowDropDownLine } from "react-icons/ri";
-import { useMediaQuery } from "react-responsive";
-import NavBar from "./Mobileviews/MobileHero";
 import { useState } from "react";
 import { VscMenu } from "react-icons/vsc";
 import { MdClose } from "react-icons/md";
 import Background from "../src/assets/desktop/Element1.png";
+import { GoArrowRight } from "react-icons/go"; 
 
 const NavLinks = () => {
   return (
@@ -49,89 +47,85 @@ function HeroPage() {
 
   return (
     <>
-     
-        <>
-          <header className="bg-[#043873] h-[92px] w-full flex   justify-between pt-[16px] pb-[16px] pl-[200px] pr-[200px]  max-xl:pr-[32px] max-xl:pl-[32px] items-center text-white ">
-            <img className="" src={logo} alt="Whitepace-logo" />
-            <nav className="2xl:block  max-[1152px]:hidden">
-              <ul className="w-full flex gap-8">
-                <li>
-                  <a href="#" className="flex  items-center ">
-                    Products
-                    <RiArrowDropDownLine className="text-4xl" />
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="flex items-center gap-1 ">
-                    Solutions
-                    <RiArrowDropDownLine className="text-4xl" />
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="flex  items-center gap-1 ">
-                    Resources
-                    <RiArrowDropDownLine className="text-4xl" />
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="flex  items-center gap-1">
-                    Pricing
-                    <RiArrowDropDownLine className="text-4xl" />
-                  </a>
-                </li>
-              </ul>
-            </nav>
+      <>
+        <header className="bg-[#043873] h-17 w-full flex items-center  justify-between px-6   text-white border-b lg:py-11 lg:px-6">
+          <img className="" src={logo} alt="Whitepace-logo" />
+          <nav className="hidden  xl:block ">
+            <ul className="w-full flex gap-8">
+              <li>
+                <a href="#" className="flex  items-center ">
+                  Products
+                  <RiArrowDropDownLine className="text-4xl" />
+                </a>
+              </li>
+              <li>
+                <a href="#" className="flex items-center gap-1 ">
+                  Solutions
+                  <RiArrowDropDownLine className="text-4xl" />
+                </a>
+              </li>
+              <li>
+                <a href="#" className="flex  items-center gap-1 ">
+                  Resources
+                  <RiArrowDropDownLine className="text-4xl" />
+                </a>
+              </li>
+              <li>
+                <a href="#" className="flex  items-center gap-1">
+                  Pricing
+                  <RiArrowDropDownLine className="text-4xl" />
+                </a>
+              </li>
+            </ul>
+          </nav>
 
-            <div className="flex gap-8  place-self-end">
-              <button className="w-[126px] h-[60px] bg-[#FFE492] rounded-[7px] text-black">
-                Login
-              </button>
-              <button className="w-[227px] h-[60px] bg-[#4F9CF9] rounded-[10px]">
-                Try Whitepace free
-              </button>
-              <button
-                className="nav-btn hidden max-[1152px]:block "
-                onClick={() => setClick(!click)}
-              >
-                {click ? <MdClose /> : <VscMenu />}
-                {click && (
-                  <div className=" w-full">
-                    <NavLinks />
-                  </div>
-                )}
+          <div className="flex gap-8  ">
+            <button className="w-[126px] h-[60px] bg-[#FFE492] rounded-[7px] text-black hidden lg:block">
+              Login
+            </button>
+            <button className="w-[227px] h-[60px] bg-[#4F9CF9] rounded-[10px] hidden lg:block">
+              Try Whitepace free
+            </button>
+            <button
+              className="nav-btn block xl:hidden "
+              onClick={() => setClick(!click)}
+            >
+              {click ? <MdClose /> : <VscMenu className="text-3xl" />}
+              {click && (
+                <div className=" w-full">
+                  <NavLinks />
+                </div>
+              )}
+            </button>
+          </div>
+        </header>
+
+        <main className="bg-[#043873] flex flex-col w-full h-full items-center lg:justify-center  lg:flex-row  gap-15 py-20 px-4    lg:py-35  relative     text-white  ">
+          <div>
+            <img
+              className="absolute object-cover inset-0 w-full h-full "
+              src={Background}
+              alt=""
+            />
+          </div>
+
+          <section className=" flex flex-col   relative  justify-center text-center lg:text-start items-center  gap-20    lg:flex-row">
+            <div className="flex flex-col items-center lg:items-start gap-8 lg:w-full lg:h-full">
+              <p className="font-extrabold text-5xl lg:text-6xl ">
+                Get More Done with whitepace
+              </p>
+              <p className="font-light text-lg lg:leading-8 ">
+                Project management software that enables your teams to
+                collaborate, plan, analyze and manage everyday tasks
+              </p>
+              <button className="bg-[#4F9CF9]  w-58  h-17.5  rounded-xl flex place-content-center items-center text-xl gap-2">
+                Try Whitepace free <GoArrowRight />
               </button>
             </div>
-          </header>
-
-          <main className="bg-[#043873]  w-full  bg-cover  relative pt-[140px] pb-[140px] pl-[180px] pr-[180px]    max-xl:pr-[32px]  max-xl:pl-[32px]">
-            <div>
-              <img
-                className="absolute inset-0 m-auto object-center object-cover"
-                src={Background}
-                alt=""
-              />
-            </div>
-
-            <section className="relative z-10 inset-0 flex gap-12">
-              <div className="text-white">
-                <p className="font-[700] text-[64px]">
-                  Get More Done with <br />
-                  whitepace
-                </p>
-                <p className="font-[300] text-[18px] whitespace-nowrap">
-                  Project management software that enables your teams to
-                  collaborate, plan, <br /> analyze and manage everyday tasks
-                </p>
-                <button className="w-[219px] h-[63px] mt-[70px] rounded-[8px] bg-[#4F9CF9]">
-                  Try Whitepace free
-                </button>
-              </div>
-
-              <div className="bg-[#A7CEFC] w-[824px] h-[549px]"></div>
-            </section>
-          </main>
-        </>
-  
+          </section>
+          <div className="bg-[#A7CEFC] w-70 h-48  lg:w-150 lg:h-87 "></div>
+        </main>
+      </>
     </>
   );
 }
