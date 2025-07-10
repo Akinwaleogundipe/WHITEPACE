3e8db2eaed13fb914157ac39babe088b4cbaab8d
Adun <adun@users-MacBook-Pro.local>
2025-07-10 18:42:26 +0100

    Fixing project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,5 @@
-<<<<<<< HEAD
-import HeroPage from "./HeroPage.jsx";
-import WorkManagement from "./WorkManagement.jsx";
-import Extension from "./Extension.jsx";
-import Customise from "./Customise.jsx";
-import Pricing from "./Pricing.jsx";
-import DataSponsor from "./DataSponsor.jsx";
-import Applications from "./Applications.jsx";
-import FreeTrial from "./FreeTrial.jsx";
-import Testimonial from "./Testimonial.jsx";
-
-
-function App() {
-  return (
-    <>
-      <HeroPage />
-      <WorkManagement />
-      <Extension />
-      <Customise />
-      <Pricing/>  
-      <DataSponsor />      
-      <Applications />
-      <FreeTrial />    
-      <Testimonial />   
-       </>
-  );
-=======
-import "./App.css";
 import HeroPage from "./components/HeroPage.jsx";
-import WorkManagement from "./components/WorkManagemnt.jsx";
+import WorkManagement from "./components/WorkManagement.jsx";
 import Extension from "./components/Extension.jsx";
 import Customise from "./components/Customise.jsx";
 import Pricing from "./components/Pricing.jsx";
@@ -35,7 +7,6 @@ import DataSponsor from "./components/DataSponsor.jsx";
 import Applications from "./components/Applications.jsx";
 import FreeTrial from "./components/FreeTrial.jsx";
 import Testimonial from "./components/Testimonial.jsx";
-import NavBar from "./Mobileviews/MobileHero.jsx";
 
 function App() {
 	return (
@@ -51,7 +22,6 @@ function App() {
 			<Testimonial />
 		</>
 	);
->>>>>>> e0accb20a561db725c27bb7aee89417c3af44c3d
 }
 
 export default App;
